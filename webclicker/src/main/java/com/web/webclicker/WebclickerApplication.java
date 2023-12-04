f4b7aad0478da177351de2e57f737647b81babec
Pepsiqqq <pepsiqqfanta@gmail.com>
2023-12-04 22:41:01 +0200

    Created

---
+++ webclicker/src/main/java/com/web/webclicker/WebclickerApplication.java
@@ -0,0 +1,13 @@
+package com.web.webclicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclickerApplication.class, args);
+	}
+
+}
