3bb7ba97fab6c1ffd0b062b8d04eb24f94412e76
Pepsiqqq <pepsiqqfanta@gmail.com>
2024-01-29 11:13:11 +0200

    first

---
+++ src/main/java/com/kursach/webcliker/WebclikerApplication.java
@@ -0,0 +1,13 @@
+package com.kursach.webcliker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclikerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclikerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kursach/webcliker/WebclikerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursach.webcliker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclikerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
